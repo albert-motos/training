e1cd995e8b444fbeae2ca9efdff2040ff1ebdd58
foxtrot-developer <albertmotosgonzalez@gmail.com>
2017-06-29 10:50:22 +0200

    Distinct DONE

--- src/test/java/com/hightechware/training/codility/lessons/sorting/DistinctTest.java
+++ src/test/java/com/hightechware/training/codility/lessons/sorting/DistinctTest.java
@@ -1,7 +1,19 @@
 package com.hightechware.training.codility.lessons.sorting;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DistinctTest extends Distinct {
+
+    @Test
+    public void Distinct_Solution_True_Case1() {
+        assertEquals(3, solution(new int[]{2, 1, 1, 2, 3, 1}));
+    }
+
+    @Test
+    public void Distinct_Solution_False_Case1() {
+        assertNotEquals(0, solution(new int[]{2, 1, 1, 2, 3, 1}));
+    }
 
 }
